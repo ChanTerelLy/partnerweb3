275e6492a22adbccc15a0864f323dcabcbe4e74b
Chanterelly <zvezdolom1@gmail.com>
2019-10-29 14:23:21 +0300

    fix app name

--- autoping.js
+++ autoping.js
@@ -1,4 +1,5 @@
+
 var http = require("http");
 setInterval(function() {
-    http.get("http://<your app name>.herokuapp.com");
+    http.get("http://partnerweb3.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
