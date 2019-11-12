bd56246adab3a1f7b3185e76d233f60011e36cc3
Roman <r9052933642@gmail.com>
2019-11-12 21:01:24 +0300

    table for fast house

--- static/js/beeline_js/fast_house_search.js
+++ static/js/beeline_js/fast_house_search.js
@@ -15,26 +15,42 @@ document.getElementById('input-button').addEventListener('click', function (valu
             $.each(result, function (key, value) {
                 document.getElementById('result').innerHTML += `<div id="${value.s_id}">${value.city} : ${value.street_name}<\div>`;
                 $.getJSON(`/get_homes_by_street?house_id=${value.s_id}`, function (houses) {
-                    $.each(houses, function (key, house) {
-                        console.log(house.h_segment);
-                        let color_house = '';
-                        switch (house.h_segment) {
-                            case -1:
-                                color_house = 'bg-second';
-                                break;
-                            case 1:
-                                color_house = 'bg-dangerous';
-                                break;
-                            case  2:
-                                color_house = 'bg-warning';
-                                break;
-                            case  3:
-                                color_house = 'bg-success';
-                                break;
-                        }
-                        document.getElementById(value.s_id).insertAdjacentHTML('beforeend', `<a target="_blank" href="./house_info/${house.h_id}" style="border: solid black" class=${color_house}>${house.name}</a>`);
-                    });
+                    document.getElementById(value.s_id).insertAdjacentHTML('beforeend', parse_table(houses));
                 });
             })
         });
 });
+function parse_table(data){
+  var perrow = 5, // 5 items per row
+      html = "<table class='table table-hover table-bordered'><tr>";
+
+  // Loop through array and add table cells
+  for (var i=0; i<data.length; i++) {
+      let color_house = '';
+      switch (data[i].h_segment) {
+          case -1:
+              color_house = 'bg-second';
+              break;
+          case 1:
+              color_house = 'bg-dangerous';
+              break;
+          case  2:
+              color_house = 'bg-warning';
+              break;
+          case  3:
+              color_house = 'bg-success';
+              break;
+      }
+
+    html += `<td class='${color_house}'><a href='/house_info/${data[i].h_id}/' target="_blank">${data[i].name}</a></td>`;
+    // Break into next row
+    var next = i+1;
+    if (next%perrow==0 && next!=data.length) {
+      html += "</tr><tr>";
+    }
+  }
+  html += "</tr></table>";
+
+  // ATTACH HTML TO CONTAINER
+  return html;
+}
