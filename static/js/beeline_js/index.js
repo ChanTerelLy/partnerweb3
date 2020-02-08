eb9ec1a0744e3b074189e8398f11a1127ba34f25
Roman <r9052933642@gmail.com>
2020-02-08 23:18:48 +0300

    minor visual changes

--- static/js/beeline_js/index.js
+++ static/js/beeline_js/index.js
@@ -18,6 +18,8 @@ function getColorSchedule(ticket_id) {
 function call_timer() {
     $('#id_datetime').datetimepicker({
         format: 'd.m.Y H:i',
+        allowTimes: ['00:00','10:00', '11:00', '12:00','13:00', '14:00','15:00',
+            '16:00','17:00', '18:00','19:00', '20:00','21:00', '22:00'],
         dayOfWeekStart: 1,
     });
 }
