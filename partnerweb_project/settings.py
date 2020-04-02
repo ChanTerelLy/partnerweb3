5f2c9526babf44304997bbf9a54a004d9f167110
Roman <r9052933642@gmail.com>
2020-04-02 15:46:05 +0300

    add prod db config

--- partnerweb_project/settings.py
+++ partnerweb_project/settings.py
@@ -89,15 +89,8 @@ WSGI_APPLICATION = 'partnerweb_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 #
-import dj_database_url
-DATABASES = {'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+# import dj_database_url
+# DATABASES = {'default' : dj_database_url.config('HEROKU_POSTGRESQL_BLUE')}
 
 DATABASES = {
     'default': {
