fffa8419fab8618608e8b44f8348824b29b43c74
Roman <r9052933642@gmail.com>
2020-04-02 23:50:23 +0300

    mozreport

--- analytic/models.py
+++ analytic/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from territory.models import Area, Address
+from tickets_handler.models import Employer, Workers
 
-# Create your models here.
-class TicketPrice(models.Model):
+class MOZSales(models.Model):
     ticket_number = models.IntegerField()
-    price = models.IntegerField()
-    ratio = models.IntegerField()
+    creation_date = models.DateTimeField()
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    switched_date = models.DateField()
+    tariff = models.CharField(max_length=250)
+    moving = models.BooleanField()
+    conv_segment = models.CharField(max_length=200)
+    operator = models.ForeignKey(Workers, on_delete=models.CASCADE)
 
-    @classmethod
-    def set_price(cls, ticket_number, price, ratio=1):
-        cls(ticket_number, price, ratio).save()
-
-    @classmethod
-    def get_price(cls, ticket_number):
-        ticket = cls.objects.get(ticket_number)
-        return ticket.price
-
-
+class Tariff:
+    moz_name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    conv_segment = models.CharField(max_length=200)
+    extra_name = models.CharField(max_length=50)
