a495e26bf17659762cedfcb6a5115176b15c35fa
rkorytov <rkorytov@siliconrussia.com>
2020-09-21 23:21:23 +0300

    make partnerweb_parser independent

--- partnerweb_parser/__init__.py
+++
@@ -1,4 +0,0 @@
-from gevent import monkey as curious_george
-curious_george.patch_all(thread=False, select=False)
-# __import__('gevent.monkey').monkey.patch_all()
-# from requests.packages.urllib3.util.ssl_ import create_urllib3_context
